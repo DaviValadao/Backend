b6d53250a6e45e0f0eaf6d3d4d2668caf124af94
DaviValadao <V.davi.f10@gmail.com>
2025-04-24 11:44:24 -0400

    Estudo de Objeto

--- Anotaçoes_Aula.js
+++ Anotaçoes_Aula.js
@@ -1,11 +1,31 @@
 //Var nome = "nome" é possivel redeclarar a mesma variavel
+
 //const 'constante', não é alteravel o valor
+
 //let 'Varriavel''Manipulavel'
+
 // os tipos são (number,boolean,string)
+
 //typeof para aber o valor da variavel 
+
 //*Toda função tem parenteses, a propriedade não..
+
 //arry como se foçe uma lista... => var estados = ["Rio de Janeiro", "São Paulo", "Bhaia"]; *Pode guarda valores mistos(number,strings,boleano)
+
 //Para contar quantidade de elementos em um arry é ".length" depois o nome de arry
+
 // lenguagens = ["JavaScript", "PHP", "Java"]
+
 //undefined = 'uma propriedade indefina', que não recebeu um valor ou não foi declarada. *se ele fo imprimida ou usada VAI DAR ERRO!! possivel solução exemplo: ( let leve ='') 
+
 //NaN para numbers, possivel solução exemplo(let num = 0;)
+
+//Há problema em usar virgula no lugar de soma (+) pos da form com virgula pode deixar a variavel como número
+
+//--------------------------------------------------------------------------------OBJETOS----------------------------------------------------------------------------------------
+
+//Em JavaScript tudo (ou quase tudo) é um objeto, ou age como um objeto.
+
+//Qualquer valor que não seja de um tipo primitivo (uma string, um número, um booleano, um símbolo, null ou undefined) é um objeto.
+
+//
--- HelloWorld.js
+++ HelloWorld.js
@@ -1,2 +1,24 @@
 //Meu primeiro Olá Mudo em JS
 console.log("Hello World!");
+
+//-----------------------Objeto---------------------------
+let valor = 10
+let outrovalor = valor 
+console.log("Valor: " +valor)
+console.log("Outro valor: "+outrovalor)
+outrovalor = 15
+console.log("Valor: "+valor)
+console.log("Outro V alor: " + outrovalor)
+
+let + {
+    cor: "Azul"
+}
+
+let biciclta = carro 
+
+console.log("Carro: "+carro.cor)
+console.log("Bicicleta: "+biciclta.cor)
+
+biciclta.cor = "Verde"
+console.log("Carro: "+carro.cor)
+console.log("Bicicleta: "+ biciclta.cor)
